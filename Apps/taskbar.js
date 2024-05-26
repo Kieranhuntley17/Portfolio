8ced346a8671162f553b6f9c787b5889b1439724
kieranhuntley17 <kieranhuntley17@gmail.com>
2024-05-26 02:12:16 +0100

    Made nicer looking minimize buttons that suit the theme more and made them change colour depending if a window is minimized or not

--- Apps/taskbar.js
+++ Apps/taskbar.js
@@ -6,8 +6,10 @@ document.getElementById('minimised-p1').addEventListener('click', function() {
     const windowDiv = document.getElementById('window1');
     if (windowDiv.style.display === 'none' || windowDiv.style.display === '') {
         windowDiv.style.display = 'block';
+        document.getElementById('minimised-p1').style.backgroundColor = "#a7a7a7"
     } else {
         windowDiv.style.display = 'none';
+        document.getElementById('minimised-p1').style.backgroundColor = "#ccc"
     }
 });
 
@@ -15,8 +17,10 @@ document.getElementById('minimised-p2').addEventListener('click', function() {
     const windowDiv = document.getElementById('window2');
     if (windowDiv.style.display === 'none' || windowDiv.style.display === '') {
         windowDiv.style.display = 'block';
+        document.getElementById('minimised-p2').style.backgroundColor = "#a7a7a7"
     } else {
         windowDiv.style.display = 'none';
+        document.getElementById('minimised-p2').style.backgroundColor = "#ccc"
     }
 });
 
@@ -24,7 +28,9 @@ document.getElementById('minimised-about').addEventListener('click', function() 
     const windowDiv = document.getElementById('window3');
     if (windowDiv.style.display === 'none' || windowDiv.style.display === '') {
         windowDiv.style.display = 'block';
+        document.getElementById('minimised-about').style.backgroundColor = "#a7a7a7"
     } else {
         windowDiv.style.display = 'none';
+        document.getElementById('minimised-about').style.backgroundColor = "#ccc"
     }
 });
